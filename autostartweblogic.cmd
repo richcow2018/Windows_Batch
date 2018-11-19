b47f4ee3fa2ba2cda1bb072af8c1860c568971ec
richcow2018 <39473760+richcow2018@users.noreply.github.com>
2018-11-19 17:19:22 +0800

    Add files via upload

---
+++ autostartweblogic.cmd
@@ -0,0 +1,9 @@
+tasklist /FI "IMAGENAME eq java_2.exe" /FO CSV > search.log
+
+FINDSTR java_2.exe search.log > found.log
+
+FOR /F %%A IN (found.log) DO IF %%~zA EQU 0 GOTO end
+
+start "" /b "C:\Documents and Settings\pqcadmin\Desktop\WebLogic.lnk"
+
+:end
